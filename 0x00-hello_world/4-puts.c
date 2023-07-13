4199955a4a00905ea937c41920c9ab6a9225606a
David-Mark10 <dm_20167@yahoo.com>
2023-07-13 22:51:54 +0100

    My first print out command using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /* main is the entry point
  *
- * Returns success */
+ * Returns success 
+ */
  int main(void) 
  {
-   puts("Programming is like building a multilingual puzzle\n");
-   return(0);
+          puts("\"Programming is like building a multilingual puzzle\n");
+          return(0);
  
  }
