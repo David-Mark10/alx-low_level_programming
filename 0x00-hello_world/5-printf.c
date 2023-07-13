526d2e008e6e1eb23cc62f67b5eb67676cd41913
David-Mark10 <dm_20167@yahoo.com>
2023-07-13 23:10:43 +0100

    My first print out command using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a pices of art,");
+	return (0);
+}
