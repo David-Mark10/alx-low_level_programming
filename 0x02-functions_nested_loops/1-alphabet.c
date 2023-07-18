75883eeb4b5a749929e0a7ef22c90cbaec1df3a9
David-Mark10 <dm_20167@yahoo.com>
2023-07-18 11:41:32 -0700

    This is my c command to count alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include 'main.h'
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (int i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	Putchar('\n');
+	return;
