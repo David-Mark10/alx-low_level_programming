f794f269a738df054b1d2586b79e31637f093ef0
David-Mark10 <dm_20167@yahoo.com>
2023-07-18 12:53:34 -0700

    Writing command to print 10 x lower case

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * description: To print 10 times lower case alphabet
+ * Return: Always 0 (Success)
+ */
+void void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
