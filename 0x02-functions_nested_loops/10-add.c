9c4d90c4337bf4b402a951a85b9461422308ebea
David-Mark10 <dm_20167@yahoo.com>
2023-07-18 14:34:27 -0700

    This is a command to add two integer number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Check main
+ * @a: An integer number a
+ * @b: An integer number b
+ * Description: My function to add two integers and returns the result
+ * Return: Result to add a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
