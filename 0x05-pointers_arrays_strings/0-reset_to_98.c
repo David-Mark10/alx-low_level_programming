a5ad1d02ab2669f84d4de2d62797626f00b43109
David-Mark10 <dm_20167@yahoo.com>
2023-07-24 06:47:04 -0700

    My first arrays swap

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * 0-reset_to_98: command to reset the pointer 
- *
- * Return:Always 0 (success)
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ * @n: An integer input pointer
+ * Return: Nothing.
  */
 void reset_to_98(int *n)
 {
