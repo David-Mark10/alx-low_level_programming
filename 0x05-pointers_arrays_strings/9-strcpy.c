b5d9ef0b6ca7049fa50fbebcb6503952262822c3
David-Mark10 <dm_20167@yahoo.com>
2023-07-24 14:45:59 -0700

    Araays command to generate random password

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * _strcpy - a function that copies the string pointed to by src,
  * including the terminating null byte (\0),
@@ -10,9 +11,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *aux = dest;
-
-	while (*src)
-		*dest++ = *src++;
-	return (aux);
+	int inc = 0;
+	
+	while (*(src + inc) != '\0')
+	{
+		*(dest + inc) = *(src + inc);
+		inc++;
+	}
+	*(dest + inc) = '\0';
+	return (dest);
 }
