b9727789810dbc0a35ca0644f40a2688ba68c0df
David-Mark10 <dm_20167@yahoo.com>
2023-08-02 13:01:52 -0700

    My code for resolving  recursive task

---
+++ 0x04-more_functions_nested_loops/mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void main()
+{
+	int i;
+	i = 0;
+
+	for (; i <= 9; i++)
+	{
+		printf("i");
+	}
+	printf("\n");
+}
---
+++ 0x04-more_functions_nested_loops/upper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _isdigit(int c)
+{
+	int i = 0;
+	int a = 0;
+
+	for (; i <= 9; i++)
+	{
+		if (c == i)
+		{
+			a = 1;
+			printf("a");
+		}
+	return (0);
+	}
+}
