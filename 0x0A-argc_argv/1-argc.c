d516a101b90b4fbf3f50497b73e8020f5065e1b7
David-Mark10 <dm_20167@yahoo.com>
2023-08-04 15:54:58 -0700

    Still creating argc and argv codes to print output

---
+++ 0x0A-argc_argv/1-argc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print program name.
+ * @argc: number of argument.
+ * @argv: array of argument.
+ *
+ *Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
