5cea187e16a4fec586452a9252ae76bb1b8b0101
David-Mark10 <dm_20167@yahoo.com>
2023-08-17 22:25:19 -0700

    Command to check and print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,30 +1,32 @@
 #include "variadic_functions.h"
 /**
+ * print_strings - print strings
+ * @separator: separator
+ * @n: number of string
  *
- *
- *
- *
- *
- *
+ * Return: return northing
  */
 void print_strings(const char *separator, const unsigned int n, ...)
-{       
-        va_list pstrg;
-        unsigned int j;
-        char *ptr;
+{
+	va_list pstrg;
+	unsigned int j;
+	char *ptr;
 
-        va_start(pstrg, n);
-        
-        if (separator != NULL)
+	va_start(pstrg, n);
+
+	for (j = 0; j < n; j++)
 	{
-                for (j = 0; j < n; j++)
-		{
-		 	ptr = va_arg(pstrg, char);
+		ptr = va_arg(pstrg, char *);
 
-			if (ptr[j] == NULL)
-				printf(nil);
+		if (ptr == NULL)
+			printf(nil);
 
-			printf("%c", ptr[j]);
-		}
-		printf("\n");
+		else 
+			printf("%s", ptr);
 
+		if (j != (n + 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(pstrg);
+}
