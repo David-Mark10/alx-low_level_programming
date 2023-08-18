54f5efa8b13dc716ba2dfc86802a467c06ac0ba2
David-Mark10 <dm_20167@yahoo.com>
2023-08-18 03:39:01 -0700

    A function that prints argurment

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else 
 			printf("%s", ptr);
 
-		if (j != (n + 1) && separator != NULL)
+		if (j != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
