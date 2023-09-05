84c65be5047b71e4f0926c8110705040ea4ca203
David-Mark10 <dm_20167@yahoo.com>
2023-09-05 07:16:45 -0700

    Project on 0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,9 @@
+#include "lists.h"
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * print_list - function that prints all the elements of a list_t list
+ * @h: pointer to a list
  *
- * Description: singly linked list node structure
+ * Return: return the number of a node
  */
 size_t print_list(const list_t *h)
 {
